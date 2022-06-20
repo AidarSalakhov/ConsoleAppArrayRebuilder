eb367ca14305f1e4b981dc502a4feed2871e60f5
AidarSalakhov <adr-sal@yandex.ru>
2022-06-20 18:46:44 +0300

    Багфикс

--- ConsoleAppArrayRebuilder/Array2DSMethods.cs
+++ ConsoleAppArrayRebuilder/Array2DSMethods.cs
@@ -31,8 +31,6 @@ namespace ConsoleAppArrayRebuilder
 
         public static int[] AboveSecondaryDiagonalLowerThanXToArray(int x, int[,] newArray)
         {
-            int[,] result2DArray = new int[newArray.GetLength(0), newArray.GetLength(1)];
-
             List<int> list = new List<int>();
 
             for (int i = 0, z = newArray.GetLength(1) - 1; i < newArray.GetLength(0); i++, z--)
@@ -42,9 +40,6 @@ namespace ConsoleAppArrayRebuilder
                     if (newArray[i, k] < x)
                         list.Add(newArray[i,k]);
                 }  
-
-                for (int j = z; j < newArray.GetLength(1); j++)
-                    result2DArray[i, j] = newArray[i, j];
             }
 
             int[] resultArray = new int[list.Count];
